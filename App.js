cd2e70e56fa2f2c14c334ad21bc8cf64c23c1f8d
Massimiliano Fiori <massimiliano.fiori@aol.it>
2018-02-25 14:56:41 +0100

    General Update

--- App.js
+++ App.js
@@ -3,17 +3,22 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+const WEBVIEW_REF = "WEBVIEW_REF";
 import React, { Component } from 'react';
-import { WebView } from 'react-native';
 import {
+  AppRegistry,
+  TouchableOpacity,
+  WebView,
   Platform,
   StyleSheet,
   Text,
   View,
   StatusBar,
-  Navigator
+  Navigator,
+  NetInfo,
+  Alert
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,44 +27,109 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+
+//type Props = {};
+export default class App extends Component<> {
+  constructor(props) {
+    super(props);
+    this.state = { canGoBack: false };
+    this.checkConnection();
+  }
+
   render() {
     return (
-	 <WebView
-        source={{uri: 'https://www.onfarma.it/'}}
-        style={{marginTop: 20}}
-      />
-	/**
-	<Header
-		leftComponent={{ icon: 'menu', color: '#fff' }}
-		centerComponent={{ text: 'OnFarmarr', style: { color: '#fff' } }}
-		rightComponent={{ icon: 'home', color: '#fff' }}
-		/>
-      <WebView
-        source={{uri: 'https://www.onfarma.it/'}}
-        style={{marginTop: 20}}
-      />
-	  */
+      <View style={styles.container}>
+        <View style={styles.topbar}>
+          <TouchableOpacity
+            disabled={!this.state.canGoBack}
+            onPress={this.onBack.bind(this)}>
+            <Icon name='chevron-left' size={20} style={this.state.canGoBack ? styles.topbarText : styles.topbarTextDisabled} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerWeb}>
+        <WebView
+          ref={WEBVIEW_REF}
+          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+          style={{flex:1}}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          source={{uri: 'https://www.onfarma.it/'}} />
+        </View>
+      </View>
     );
+  }
+
+  onBack() {
+    this.refs[WEBVIEW_REF].goBack();
+  }
+
+  checkConnection() {
+    NetInfo.isConnected.fetch().then(isConnected => {
+    console.log('First, is ' + (isConnected ? 'online' : 'offline'));
+    // Works on both iOS and Android
+    if (isConnected==true)
+    {
+      Alert.alert(
+        'Attenzione',
+        'La connessione è assente.',
+        [
+          {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        { cancelable: false }
+      )
+    }
+    });
+  }
+
+  onNavigationStateChange(navState) {
+    this.setState({
+      canGoBack: navState.canGoBack
+    });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 15, /* Padding to push below the navigation bar */
     justifyContent: 'center',
-    alignItems: 'center',
+    //alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  containerWeb: {
+    flex: 1,
+    //paddingTop: 15, /* Padding to push below the navigation bar */
+    justifyContent: 'center',
+    //alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  topbar: {
+    height: 40,
+    paddingBottom: 10,
+    justifyContent: 'center',
+    //alignItems: 'center',
   },
+  topbarTextDisabled: {
+    color: 'gray',
+    paddingLeft: 8,
+    paddingTop: 15,
+    width: 100,
+    //position: 'absolute',
+    //flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-between',
+  },
+  topbarText: {
+    //color: 'gray',
+    paddingLeft: 8,
+    paddingTop: 15,
+    width: 100,
+    //flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
 });
